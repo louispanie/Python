566f188241bf2fbed9f7106e542aae7444cee2c4
louispanie <louis_panie@ryecountryday.org>
2025-07-03 19:29:51 +0000

    coomit3

---
+++ turtledrawing.py
@@ -0,0 +1,8 @@
+import turtle
+
+t=turtle.Turtle()
+
+hi=True
+
+while hi:
+    pass
--- war.py
+++ war.py
@@ -24,10 +24,18 @@ class deck:
     def shuffle(self):
         self.shufflepile=[]
         for i in range(52):
-            x=randint(0, len(self.pile))
-            self.shufflepile.append(self.pile[x-1])
-            self.pile.remove(self.pile[x-1])
-        self.pile=self.shufflepile
+            x=randint(0,len(self.pile)-1)
+            y=randint(0,len(self.pile)-1)
+            temp=self.pile[x]
+            self.pile[x]=self.pile[y]
+            self.pile[y]=temp
+        
+
+
+        # x=randint(0, len(self.pile))
+        #     self.shufflepile.append(self.pile[x-1])
+        #     self.pile.remove(self.pile[x-1])
+        # self.pile=self.shufflepile
 
 pile=deck()
 pile.shuffle()
@@ -42,56 +50,65 @@ for x in range(26):
     p2.append(pile.pile[0])
     pile.draw()
 game=True
-while game==True:
+while game:
+    middle = []
     middle.append(p1[0])
-    print("Player 1 plays: "+ str(p1[0]))
-    p1.remove(p1[0])
     middle.append(p2[0])
-    print("Player 2 plays: "+ str(p2[0]))
+
+    p1.remove(p1[0])
     p2.remove(p2[0])
+
+    print(f"Player 1 plays: {middle[0]}")
+    print(f"Player 2 plays: {middle[1]}")
 
     if middle[0].value>middle[1].value:
         print("Player 1 had a higher card")
-        p1.append(middle[0])
-        middle.remove(middle[0])
-        p1.append(middle[0])
-        middle.remove(middle[0])
+        for x in range(2):
+            p1.append(middle[0])
+            middle.remove(middle[0])
     elif middle[1].value> middle[0].value:
         print("Player 2 had a higher card")
-        p2.append(middle[0])
-        middle.remove(middle[0])
-        p2.append(middle[0])
-        middle.remove(middle[0])
-    elif middle[1].value== middle[0].value:
-        if len(p1)>4 and len(p2)>4:
+        for x in range(2):
+            p2.append(middle[0])
+            middle.remove(middle[0])
+    else:
+        if len(p1)>=4 and len(p2)>=4:
             print("WAR")
+            for a in range(3):
+                middle.append(p1[0])
+                p1.remove(p1[0])
+                middle.append(p2[0])
+                p2.remove(p2[0])
+
             middle.append(p1[0])
-            print("Player 1 plays: "+ str(p1[0]))
-            p1.remove(p1[0])
             middle.append(p2[0])
-            print("Player 2 plays: "+ str(p2[0]))
+
+            p1.remove(p1[0])            
             p2.remove(p2[0])
-            if middle[0].value>middle[1].value:
-                print("Player 1 had a higher card")
-                for i in range(5):
-                    p1.append(middle[0])
-                    middle.remove(middle[0])
-            elif middle[1].value> middle[0].value:
+
+            if middle[-2].value > middle[-1].value:
+                print("Player 1 wins the war")
+                p1+=middle
+            elif middle[-2].value<middle[-1].value:
                 print("Player 2 had a higher card")
-                for j in range(5):
-                    p2.append(middle[0])
-                    middle.remove(middle[0])
+                p2+=middle
+            else:
+                print("War results in another tie!")
+                middle = [] 
         else:
             if len(p1)<4:
                 print("Player 1 lost")
-                break
+                game=False
             elif len(p2)<4:
                 print("Player 2 lost")
-                break
+                game=False
 
-
-
-
+    if len(p1)==0:
+        print("Player 1 lost")
+        game=False
+    elif len(p2)==0:
+        print("Player 2 lost")
+        game=False
 
     print("Player 1 has: " + str(len(p1))+" cards")
     print("Player 2 has: " + str(len(p2))+" cards")
